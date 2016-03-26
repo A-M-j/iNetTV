cb981510ab9f7b8968bbeea18dbd9646e2afcd31
Sergii Aleksieiev <saleksieiev@sergiis-macbook-air.local>
2016-03-26 18:12:03 +0200

    Simple review of MainTabBarController

--- ez-iptv/MainTabBarController.swift
+++ ez-iptv/MainTabBarController.swift
@@ -3,14 +3,12 @@ import AVKit
 
 class MainTabBarController : UITabBarController {
 
-	//MARK: Vars
 	let selectedTabKey = "selectedTabKey"
 	
-	//MARK: Private
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+
+        let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
 		var newViewControllers = [UIViewController]()
 		var i = 0
 		
@@ -21,26 +19,23 @@ class MainTabBarController : UITabBarController {
 			vc.tabBarItem = UITabBarItem(title: region, image: nil, tag: i)
 			vc.region = region
 			newViewControllers.append(vc)
-			i++
+			i = i + 1
 		}
 		
 		self.viewControllers = newViewControllers
 	}
 	
-	
-	override func viewDidAppear(animated: Bool)
-	{
-		let selectedTab = NSUserDefaults.standardUserDefaults().integerForKey(selectedTabKey)
+	override func viewDidAppear(animated: Bool) {
 
+        let selectedTab = NSUserDefaults.standardUserDefaults().integerForKey(selectedTabKey)
 		self.selectedIndex = selectedTab
 	}
 	
-	override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem)
-	{
-		let selectedTab = tabBar.items?.indexOf(item)
+	override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        
+        let selectedTab = tabBar.items?.indexOf(item)
 		NSUserDefaults.standardUserDefaults().setInteger(selectedTab!, forKey: "selectedTabKey")
 		NSUserDefaults.standardUserDefaults().synchronize()
 	}
-
 }
 
